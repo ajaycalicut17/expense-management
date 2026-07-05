88d08c8cd38a2743295a8dda2164733dab1ed0cc
GitHub Action <actions@github.com>
2026-07-05 18:56:28 +0000

    [pint] Pint fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,6 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use App\Enums\RoleEnum;
-use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Casts\Attribute;
