07fcdd3f60bf886b420c6f7b47fed73b9355e639
Ajay A <33749143+ajaycalicut17@users.noreply.github.com>
2026-07-05 21:07:40 +0530

    Update rector.php

--- rector.php
+++ rector.php
@@ -14,7 +14,7 @@ return RectorConfig::configure()
         __DIR__.'/routes',
         __DIR__.'/tests',
     ])
-    ->withPhpSets()
+    ->withPhpSets(php85: true)
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
     ->withCodeQualityLevel(0);
