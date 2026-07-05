afaf86c586c7155fbf7842a320046e5977375c6d
Ajay A <33749143+ajaycalicut17@users.noreply.github.com>
2026-07-05 22:07:06 +0530

    Update rector.php

--- rector.php
+++ rector.php
@@ -16,5 +16,5 @@ return RectorConfig::configure()
     ])
     ->withPhpSets(php85: true)
     ->withTypeCoverageLevel(2)
-    ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0);
+    ->withDeadCodeLevel(2)
+    ->withCodeQualityLevel(2);
