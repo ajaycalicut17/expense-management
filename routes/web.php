0a08cee871fb970375768f1d4743ce0abb19cd01
Ajay A <33749143+ajaycalicut17@users.noreply.github.com>
2026-08-25 18:16:59 +0530

    health check endpoint (#79)
    * health check endpoint
    
    * [rector] Rector fixes
    
    * [pint] Pint fixes
    
    * browser test removed
    
    * test
    
    * Revert "test"
    
    This reverts commit 40930a075a08249345b530bcb5eaf7ea8e050016.
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function (): void {
 
+    Route::get('/health', fn () => response()->json(['status' => 'ok']))->name('health');
+
     Route::controller(LoginController::class)->group(function (): void {
         Route::get('/', 'index')->name('index');
         Route::post('/login', 'login')->name('login');
